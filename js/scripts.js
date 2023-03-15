a2c81d21626cfb6571d8e1f1b4006e96e26c6181
juli20008 <mia.li.mba@gmail.com>
2023-03-14 22:01:34 -0400

    Add pokemonList array

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,17 @@
-alert('Hello world');
-let favoriteFood = 'Sushi';
-document.write(favoriteFood);
+let pokemonList = [];
+
+pokemonList[0]={
+    name:'Bulbasaur',
+    height:7,
+    types:['grass', 'poison']
+};
+pokemonList[1]={
+    name:'Charmander',
+    height:10,
+    types:['grass', 'poison']
+};
+pokemonList[2]={
+    name:'Venusaur',
+    height:20,
+    types:['grass', 'poison']
+};
