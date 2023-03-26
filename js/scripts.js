d4eeffe13c09dff27fe2cf9909234efcb50d9a14
juli20008 <mia.li.mba@gmail.com>
2023-03-26 11:09:50 -0400

    change for to forEach loop

--- js/scripts.js
+++ js/scripts.js
@@ -16,11 +16,21 @@ pokemonList[2]={
     types:['grass', 'poison']
 };
 
-for (let i = 0; i < pokemonList.length; i++) {
+/* for (let i = 0; i < pokemonList.length; i++) {
          if (pokemonList[i].height > 10) {
                  document.write(`${pokemonList[i].name} (height: ${pokemonList[i].height}) - Wow! That's big!`+ '<br>'+ '<br>');
          } else {
             document.write(`${pokemonList[i].name} (height: ${pokemonList[i].height})`+ '<br>'+ '<br>');
          } 
   }
+ */
 
+function myLoopFunction(alist){
+    if (alist.height > 10) {
+        document.write(`${alist.name} (height: ${alist.height}) - Wow! That's big!`+ '<br>'+ '<br>');
+} else {
+   document.write(`${alist.name} (height: ${alist.height})`+ '<br>'+ '<br>');
+} 
+}
+
+pokemonList.forEach(myLoopFunction);
